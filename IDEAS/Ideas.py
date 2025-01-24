b77a668c52cd3bd1fdb70eac39bfddd910d5d59e
HoangTran223 <tranvuonghoang204@gmail.com>
2025-01-24 19:22:14 +0700

    Update

--- IDEAS/Ideas.py
+++ IDEAS/Ideas.py
@@ -69,6 +69,7 @@ class IDEAS(nn.Module):
         
 
         ##
+        self.top_words_dict = None
         self.vocab = vocab
         self.matrixP = None
         self.DT_ETP = DT_ETP(weight_loss_DT_ETP, DT_alpha)
