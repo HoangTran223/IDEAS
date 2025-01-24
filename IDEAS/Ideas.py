2cd2cc01cbd5a58068a6c172613116fb956def5b
HoangTran223 <tranvuonghoang204@gmail.com>
2025-01-24 19:53:01 +0700

    Update

--- IDEAS/Ideas.py
+++ IDEAS/Ideas.py
@@ -173,7 +173,7 @@ class IDEAS(nn.Module):
         return top_words
 
     def init_topic_top_words(self, vocab, top_words_dict, num_top_words = 15):
-        self.topic_top_words = self.export_top_words(beta, vocab, num_top_words)
+        self.topic_top_words = self.export_top_words(vocab, num_top_words)
     
     def get_contrastive_loss_large_clusters(sinit_topic_top_wordself):
 
