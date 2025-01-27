f3a38cab143f5054f260b1697920dc206ae99d5a
HoangTran223 <tranvuonghoang204@gmail.com>
2025-01-27 15:42:45 +0700

    Update

--- IDEAS/Ideas.py
+++ IDEAS/Ideas.py
@@ -384,7 +384,7 @@ class IDEAS(nn.Module):
     #     return cost
     def pairwise_euclidean_distance(self, x, y):
         cost = torch.sum(x ** 2, axis=1, keepdim=True) + \
-            torch.sum(y ** 2, dim=1) - 2 * torch.matmul(x, y.t)
+            torch.sum(y ** 2, dim=1) - 2 * torch.matmul(x, y.t())
         return cost
 
 
